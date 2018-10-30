8f5043f36490669423faf5579550fe7efa9e201d
evgeniysolodov <solodov.exceedteam@gmail.com>
2018-10-30 16:58:30 +0300

    my commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,45 @@
-var quest = prompt('Writing "Hello world!"');
-if (quest == "Hello world!") {
-    alert('Hi man!')
-} else {
-    alert('goodbye goodbye!')
+window.onload = function(){
+        
+        var todoList = [];
+
+        if (localStorage.getItem("todo") != undefined) {
+            todoList = JSON.parse(localStorage.getItem("todo"));
+            out();
+        }
+        
+        document.getElementById('add').onclick = function(){
+            
+            var inp = document.getElementById('in').value;
+            if (inp){
+            var temp   = {};
+            temp.todo  = inp;
+            temp.check = false;
+    
+            var i = todoList.length;
+            todoList[i] = temp;
+    
+            out();
+        
+    
+            localStorage.setItem('todo', JSON.stringify(todoList) );
+            document.getElementById("in").value = "";
+        }
+    }
+
+    function out () {
+		var out ='';
+
+		for (var key in todoList) {
+
+			if ( todoList[key].check == true ) {
+				out += '<input type="checkbox" class="che" checked>'
+			} else {
+				out += '<input type="checkbox" class="che" >'
+			}
+
+			out += todoList[key].todo +'<br>';
+		}
+		document.getElementById('out').innerHTML = out;
+	}
+
 }
