6f41aaddbc7ba5f6c66a1c1c5ed331ed55186946
evgeniysolodov <solodov.exceedteam@gmail.com>
2018-10-31 17:54:07 +0300

    ddddd

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,41 @@
 window.onload = function(){
         
-        var todoList = [];
+    var todoList = [];
+    var i = 0;
 
-        if (localStorage.getItem("todo") != undefined) {
-            todoList = JSON.parse(localStorage.getItem("todo"));
-            out();
-        }
-        
-        document.getElementById('add').onclick = function(){
-            
-            var inp = document.getElementById('in').value;
-            if (inp){
+    if (localStorage.getItem("todo") != undefined) {
+        todoList = JSON.parse(localStorage.getItem("todo"));
+        out();
+    }
+     
+    document.getElementById('add').onclick = function() {
+    
+        var inp = document.getElementById('in').value;
+        if (inp){
             var temp   = {};
             temp.todo  = inp;
             temp.check = false;
-    
-            var i = todoList.length;
-            todoList[i] = temp;
+            //var i = todoList.length;
+            temp.id = i;
+            todoList.push(temp);
+            //var i = ;
+            console.log(temp);
     
             out();
         
-    
+            i++;
             localStorage.setItem('todo', JSON.stringify(todoList) );
             document.getElementById("in").value = "";
+            
         }
     }
 
+    document.getElementsByClassName('che').onclick = function(e) {
+        console.log('eee', e)
+    }
+
     function out () {
-		var out ='';
+		var out = '';
 
 		for (var key in todoList) {
 
